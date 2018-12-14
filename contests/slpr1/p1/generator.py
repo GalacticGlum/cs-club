689381adf7c48458b6c7281ac5bc049424c6ebf3
GalacticGlum <nerftactic09@gmail.com>
2018-12-14 12:02:25 -0500

    Added slpr p1 data (moved old p1 to p0)

--- contests/slpr1/p1/generator.py
+++ contests/slpr1/p1/generator.py
@@ -1,21 +1,15 @@
-from numpy.random import randint
-
-# constants
-MIN_A = -10
-MAX_A = 10
-MAX_B = 10
-MAX_TESTS = 10000
+from random import randint
 
 test_cases = int(input('Test case amount: '))
 test_case_prefix = input('Test case file prefix: ')
-t_values = sorted([randint(low=1,high=MAX_TESTS+1) for i in range(test_cases)])
 for i in range(1,test_cases+1):
     with open(f'{test_case_prefix}.{i}.in', 'w+') as input_file, \
         open(f'{test_case_prefix}.{i}.out','w+') as output_file:
 
-        input_file.write(f'{t_values[i-1]}\n')
-        for j in range(t_values[i-1]):
-            a = randint(low=MIN_A,high=MAX_A+1)
-            b = randint(low=1,high=MAX_B)
-            input_file.write(f'{a} {b}\n')
-            output_file.write(f'{a**b}\n')
+        k = randint(1,50)
+        n = randint(1,50)
+        r = randint(1,100)
+        input_file.write(f'{k} {n}\n{r}')
+
+        fv = k*((1+r/100)**n-1)/(r/100)
+        output_file.write(str(round(fv,2)))
