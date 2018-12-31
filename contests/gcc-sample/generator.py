39a5718e23acba5b209669df2ac2dc1739044cf5
GalacticGlum <nerftactic09@gmail.com>
2018-12-31 18:01:27 -0500

    Added guesstimate challenge contest sample problem

---
+++ contests/gcc-sample/generator.py
@@ -0,0 +1,30 @@
+from random import randint, random
+
+def uniform_random_list(a, b, n):
+    x = a
+    result = []
+    for i in range(n, 0, -1):
+        x += (b-x) * (1-pow(random(), 1. / i))
+        result.append(int(x))
+
+    return result
+
+min_n = int(input('Min N: '))
+max_n = int(input('Max N: '))
+test_cases = int(input('Test case amount: '))
+test_case_prefix = input('Test case file prefix: ')
+
+n_values = uniform_random_list(min_n,max_n+1,test_cases)
+for i in range(1,test_cases+1):
+    with open(f'{test_case_prefix}.{i}.in', 'w+') as input_file, \
+        open(f'{test_case_prefix}.{i}.out','w+') as output_file:
+
+        n = n_values[i-1]
+        values = [randint(1,100) for i in range(n)]
+        values_str = ' '.join(str(i) for i in values)
+        input_file.write(f'{n}\n{values_str}\n')
+
+        for v in values:
+            o = 'yes' if v == 2 else 'no'
+            output_file.write(f'{o}\n')
+            
--- contests/nye18/p6/p6_solution.py
+++ contests/nye18/p6/p6_solution.py
@@ -1,33 +1,31 @@
-def solve(n,m,k,tracks):
-    grid_points = 0
-    mapper = {}
-    for _ in range(k):
-        r, c1, c2 = tracks[_]
-        if r in mapper:
-            mapper[r].append((c1, c2))
-        else:
-            mapper[r] = [(c1, c2)]
-    for k in mapper:
-        temp = mapper[k]
-        temp.sort()
-        begin = temp[0][0]
-        end = temp[0][1]
-        points = 0
-        for i in range(1, len(temp)):
-            if temp[i][0] > end:
-                points += end - begin + 1
-                begin = temp[i][0]
-                end = temp[i][1]
-            else:
-                end = max(end, temp[i][1])
-        points += end - begin + 1
-        grid_points += points
-    return m*n - grid_points  
-
 n,m,k=map(int,input().split())
-tracks=[]
-for i in range(k):
+
+tracks = {}
+for _ in range(k):
     r,a,b=map(int,input().split())
-    tracks.append((r,a,b))
-    
-print(solve(n,m,k,tracks))
+    if r not in tracks:
+        tracks[r] = []
+
+    tracks[r].append((a, b))
+
+occupied = 0
+for k in tracks:
+    intervals = tracks[k]
+    intervals.sort()
+
+    start = temp[0][0]
+    end = temp[0][1]
+    points = 0
+
+    for i in range(1, len(intervals)):
+        if intervals[i][0] > end:
+            points += end - start + 1
+            begin = intervals[i][0]
+            end = intervals[i][1]
+        else:
+            end = max(end, intervals[i][1])
+
+    points += end - start + 1
+    occupied += points
+
+print(m*n - occupied)  
