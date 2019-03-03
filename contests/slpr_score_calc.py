3f60a44ab6e179bf4095e7af0e66f895b0f6609e
GalacticGlum <nerftactic09@gmail.com>
2019-03-03 00:10:58 -0500

    Updated score calculation script

--- contests/slpr_score_calc.py
+++ contests/slpr_score_calc.py
@@ -7,10 +7,12 @@ parser.add_argument('-c', '--contests', type=str, nargs='+', required=True, help
 parser.add_argument('-w', '--weights', type=float, nargs='+', required=True, help='The weight for each contest')
 parser.add_argument('-d', '--domain', type=str, default='https://ssoj.ca/', help='The base domain.')
 parser.add_argument('-api', '--apiurl', type=str, default='/api/contest/info/', help='The API URL for contests.')
+parser.add_argument('-uapi', '--userapiurl', type=str, default='/api/user/info/', help='The API URL for a user resource.')
 parser.add_argument('-rhtml', '--rankinghtmlurl', type=str, default='/contest/{0}/ranking', help='The URL for the ranking HTML; compatible with string formatting, "{0}" is the contest code.')
 parser.add_argument('-rname', '--rankingjsonname', type=str, default='rankings', help='The JSON field name for the rankings in the contest API JSON object.')
 parser.add_argument('-tlname', '--timelimitjsonname', type=str, default='time_limit', help='The JSON field name for the time limit in the contest API JSON object.')
 parser.add_argument('-s', '--scorescale', type=int, default=100, help='The score scale.')
+parser.add_argument('-o', '--organizations', nargs='+', type=int, required=True, help='The id(s) of the allowed organizations.')
 args = parser.parse_args()
 
 def get_contest_scores(contest_code):
@@ -32,6 +34,14 @@ def get_contest_scores(contest_code):
         max_distance = (max_score**2 + max_cumtime**2)**0.5
 
         for ranking in rankings:
+            # get user information
+            user_resource_path = urllib.parse.urljoin(urllib.parse.urljoin(args.domain, args.userapiurl), ranking['user'])
+            with urllib.request.urlopen(user_resource_path) as user_url:
+                user_data = json.loads(user_url.read().decode())
+
+            if not any(x in user_data['organizations'] for x in args.organizations): continue
+                
+
             if ranking['points'] == 0:
                 score = 0
             else:
