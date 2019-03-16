76a745c9f87be204ec22a6ce657b869be425852e
GalacticGlum <nerftactic09@gmail.com>
2019-03-16 11:36:09 -0400

    Added 50pb1 p5

---
+++ contests/50pb1/p5/generator.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <string>
+#include <random>
+#include <ctime>
+#include <vector>
+#include <cmath>
+#include <type_traits>
+#include <algorithm>
+#include <iomanip>
+#include <numeric>
+
+class Random
+{
+public:
+	static void Initialize() { Initialize(static_cast<unsigned int>(time(NULL))); }
+	static void Initialize(const unsigned int seed) { m_RandomEngine = std::mt19937(seed); }
+
+	template <bool is_integral, typename T> struct uniform_distribution_selector;
+	template <typename T> struct uniform_distribution_selector<true, T>
+	{
+		using type = std::uniform_int_distribution<T>;
+	};
+	template <typename T> struct uniform_distribution_selector<false, T>
+	{
+		using type = std::uniform_real_distribution<T>;
+	};
+
+	template<typename T>
+	static T Range(T min, T max)
+	{
+		using uniform_distribution_type = typename uniform_distribution_selector<std::is_integral<T>::value, T>::type;
+		return (uniform_distribution_type(min, max))(m_RandomEngine);
+	}
+
+    template<typename T>
+	static std::vector<T> UniformVector(T a, T b, T n)
+    {
+        double x = a;
+        std::vector<T> result;
+
+        for (T i = n; i >= 0; --i)
+        {
+            x += (b - x) * (1 - std::pow(Value(), 1.0 / i));
+            result.push_back(static_cast<T>(std::floor(x)));
+        }
+
+        return result;
+    }
+
+	template <typename T>
+	static T Choose(T begin, T end)
+	{
+		unsigned long long k = Range(0, std::distance(begin, end)-1);
+		std::advance(begin, k);
+		return begin;
+	}
+
+	template <typename T>
+	static std::vector<T> Vector(T min, T max, int n)
+	{
+		std::vector<T> result;
+		for (int i = 0; i < n; ++i)
+		{
+			result.push_back(Range(min, max));
+		}
+
+		return result;
+	}
+
+	static float Value() { return Range<float>(0.0f, 1.0f); }
+private:
+	static std::mt19937 m_RandomEngine;
+};
+
+std::mt19937 Random::m_RandomEngine;
+
+std::vector<std::string> split(const std::string& value, const std::string delimiter)
+{
+  std::string s(value);
+  std::vector<std::string> result;
+  
+  size_t pos = 0;
+  std::string token;
+  while ((pos = s.find(delimiter)) != std::string::npos)
+  {
+      token = s.substr(0, pos);
+      result.push_back(token);
+      s.erase(0, pos + delimiter.length());
+  }
+  
+  result.push_back(s);
+  return result;
+}
+
+int main(int argc, char** argv) 
+{
+    std::ios_base::sync_with_stdio(NULL);
+    std::cin.tie(0);
+    std::cout.tie(0);
+
+	int n = Random::Range(1, 10000);
+	std::vector<double> values(n);
+
+	std::cout << n << "\n";
+	for(int i = 0; i < n; ++i)
+	{
+		values[i] = static_cast<double>(Random::Range(-1000000, 1000000));
+		std::cout << values[i] << " ";
+	}
+
+	std::cout << "\n";
+
+	std::sort(values.begin(), values.end());
+	double mean = std::accumulate(values.begin(), values.end(), 0) / (double)n;
+	double median = n % 2 != 0 ? values[(n+1)/2-1] : (values[n/2-1]+values[(n+2)/2-1])/2;
+	double sigma = 0;
+	for(auto& v : values)
+	{
+		sigma += std::pow(v - mean, 2);
+	}
+
+	sigma = std::sqrt(sigma / (n - 1));
+	std::cerr << std::fixed << std::setprecision(2) << mean << " " << median << " " << sigma << "\n";
+}
